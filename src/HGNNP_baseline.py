fix data leakage

src/HGNNP_baseline.py
@@ -26,9 +26,8 @@ def train(
     st = time.time()
     optimizer.zero_grad()
     outs = net_model(nodes_features, graph)
-    # outs: torch.Size([7403, 2000]) 就是7403个点，每个点有 2000个 features
-    # labels: torch.Size([7403, 2000]) 就是7403个点，和每个点的真实的 2000个 features
-    outs, labels = outs[train_idx], labels[train_idx]
+
+    outs = outs[train_idx]
     loss = F.cross_entropy(outs, labels)
     loss.backward()
     optimizer.step()
@@ -42,7 +41,7 @@ def validation(net_model, nodes_features, graph, labels, validation_idx):
 
     outs = net_model(nodes_features, graph)
 
-    outs, labels = outs[validation_idx], labels[validation_idx]
+    outs = outs[validation_idx]
 
     val_res = ndcg_score(labels.cpu().numpy(), outs.cpu().numpy())
 
@@ -60,7 +59,7 @@ def test(net_model, nodes_features, graph, labels, test_idx):
 
     outs = net_model(nodes_features, graph)
 
-    outs, labels = outs[test_idx], labels[test_idx]
+    outs = outs[test_idx]
 
     test_res = ndcg_score(labels.cpu().numpy(), outs.cpu().numpy())
 
@@ -80,7 +79,9 @@ if __name__ == "__main__":
     data_loader = DataLoaderAttribute("Disease", "attribute prediction dataset")
 
     # get the labels - the original nodes features
-    labels = torch.FloatTensor(data_loader["raw_nodes_features"])
+    train_labels = data_loader["train_labels"]
+    validation_labels = data_loader["validation_labels"]
+    test_labels = data_loader["test_labels"]
 
     # get the train,val,test nodes features
     train_nodes_features = torch.FloatTensor(data_loader["train_nodes_features"])
@@ -115,16 +116,18 @@ if __name__ == "__main__":
     )
 
     # set the device
-    train_nodes_features, validation_nodes_features, test_nodes_features, labels = (
+    train_nodes_features, validation_nodes_features, test_nodes_features = (
         train_nodes_features.to(device),
         validation_nodes_features.to(device),
         test_nodes_features.to(device),
-        labels.to(device),
     )
+    train_labels = train_labels.to(device)
+    validation_labels = validation_labels.to(device)
+    test_labels = test_labels.to(device)
     hyper_graph = hyper_graph.to(device)
     net_model = net_model.to(device)
 
-    print("HGNN Baseline")
+    print("HGNNP Baseline")
 
     # start to train
     for epoch in range(200):
@@ -134,7 +137,7 @@ if __name__ == "__main__":
             net_model,
             train_nodes_features,
             hyper_graph,
-            labels,
+            train_labels,
             train_mask,
             optimizer,
             epoch,
@@ -143,7 +146,11 @@ if __name__ == "__main__":
         if epoch % 1 == 0:
             with torch.no_grad():
                 validation(
-                    net_model, validation_nodes_features, hyper_graph, labels, val_mask
+                    net_model,
+                    validation_nodes_features,
+                    hyper_graph,
+                    validation_labels,
+                    val_mask,
                 )
 
-    test(net_model, test_nodes_features, hyper_graph, labels, test_mask)
+    test(net_model, test_nodes_features, hyper_graph, test_labels, test_mask)